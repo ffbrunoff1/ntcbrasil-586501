50e0b92f334e81a2374448e25acf4a8a1a64cef2
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-07-26 17:26:35 -0300

    Adicionar tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#51A8DC',
+        accent: '#0A3D62', // A darker blue for contrast and text
+        neutral: '#F3F4F6', // A light gray for backgrounds
+        'dark-text': '#1f2937',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.8s ease-out forwards',
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
